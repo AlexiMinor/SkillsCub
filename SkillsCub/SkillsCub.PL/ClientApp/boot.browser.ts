807ab0f19cc775e1dc6c764caeaa261652ba17b1
Unknown <aleximinor1310@gmail.com>
2017-11-23 00:30:53 +0300

    Trying edit of the webpack
    NOT STABLE V.0.0.2

--- SkillsCub/SkillsCub.PL/ClientApp/boot.browser.ts
+++ SkillsCub/SkillsCub.PL/ClientApp/boot.browser.ts
@@ -4,6 +4,26 @@ import 'bootstrap';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.browser.module';
+import './assets/css/materialadmin.css';
+import './assets/css/font-awesome.min.css';
+import './assets/css/material-design-iconic-font.min.css';
+
+
+import './assets/js/libs/jquery/jquery-1.11.2.min.js';
+import './assets/js/libs/jquery/jquery-migrate-1.2.1.min.js';
+import './assets/js/libs/bootstrap/bootstrap.min.js';
+import './assets/js/libs/spin.js/spin.min.js';
+import './assets/js/libs/autosize/jquery.autosize.min.js';
+import './assets/js/libs/nanoscroller/jquery.nanoscroller.min.js';
+import './assets/js/core/source/App.js';
+import './assets/js/core/source/AppNavigation.js';
+import './assets/js/core/source/AppOffcanvas.js';
+import './assets/js/core/source/AppCard.js';
+import './assets/js/core/source/AppForm.js';
+import './assets/js/core/source/AppNavSearch.js';
+import './assets/js/core/source/AppVendor.js';
+import './assets/js/core/demo/Demo.js';
+import './assets/js/core/demo/DemoLayouts.js';
 
 if (module.hot) {
     module.hot.accept();
