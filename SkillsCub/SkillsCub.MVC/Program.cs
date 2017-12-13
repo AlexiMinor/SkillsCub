0d62142d5c5be08d8549edbe40cff0461bbb31de
Unknown <aleximinor1310@gmail.com>
2017-12-14 00:22:41 +0300

    Added Application Insights
    v.0.0.4

--- SkillsCub/SkillsCub.MVC/Program.cs
+++ SkillsCub/SkillsCub.MVC/Program.cs
@@ -19,6 +19,7 @@ namespace SkillsCub.MVC
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
