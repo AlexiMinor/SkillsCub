0b9793f50e138e860a8badca42cde4b362e20a39
Unknown <aleximinor1310@gmail.com>
2017-12-30 00:11:11 +0300

    Small redesigned. Added index without info, added request page
    v.0.0.7

---
+++ SkillsCub/SkillsCub.MVC/Controllers/RequestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkillsCub.MVC.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
