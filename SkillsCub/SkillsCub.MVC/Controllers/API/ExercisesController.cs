4b448c10edfb3a8ddb896c285ff5dcd15515d33e
AlexiMinor <aleximinor1310@gmail.com>
2018-10-24 23:36:47 +0300

    Add method to controller for import implementation

--- SkillsCub/SkillsCub.MVC/Controllers/API/ExercisesController.cs
+++ SkillsCub/SkillsCub.MVC/Controllers/API/ExercisesController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SkillsCub.DataLibrary.Entities.Implementation;
 using SkillsCub.DataLibrary.Repositories.Interfaces;
@@ -12,21 +14,37 @@ namespace SkillsCub.MVC.Controllers.API
     public class ExercisesController : ControllerBase
     {
         private readonly ITelegramLogger _telegramLogger;
-        private readonly IRepository<Exercise> _repository;
+        private readonly IRepository<Exercise> _exerciseRepository;
+        private readonly IRepository<Course> _courseRepository;
 
-        public ExercisesController(IRepository<Exercise> repository, 
+
+
+        public ExercisesController(IRepository<Exercise> exerciseRepository, 
             ITelegramLogger telegramLogger)
         {
-            _repository = repository;
+            _exerciseRepository = exerciseRepository;
             _telegramLogger = telegramLogger;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get(Guid id)
         {
-            var result = await _repository.GetById(id);
+            var result = await _exerciseRepository.GetById(id);
 
             return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTasksByTeacher(string id)
+        {
+            var coursesOfTeacher = (await _courseRepository
+                    .FindBy(course
+                        => course.TeacherId.Equals(id))).ToList();
+
+            var exercises = (await _exerciseRepository.FindBy(exercise =>
+                coursesOfTeacher.Any(course => course.Id.Equals(exercise.Id)))).ToList();
+
+            return Ok(exercises);
         }
     }
 }
