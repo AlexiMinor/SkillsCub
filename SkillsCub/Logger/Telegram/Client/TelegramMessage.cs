7fdf50322c2d7c7b19112d18cbf3afcb03ad5c0c
Headkliff <nikitasheiko14@gmail.com>
2018-11-08 13:14:40 +0300

    Telegram Logger

---
+++ SkillsCub/Logger/Telegram/Client/TelegramMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Logger.Telegram.Client
+{
+    public class TelegramMessage
+    {
+        public TelegramMessage(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
+    }
+}
---
+++ SkillsCub/Logger/Telegram/TelegramSinkExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Logger.Telegram
+{
+    static class TelegramSinkExtension
+    {
+        public static LoggerConfiguration Telegram(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string token,
+            string chatId,
+            TelegramSink.RenderMessageMethod renderMessageImplementation = null,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            IFormatProvider formatProvider = null
+        )
+        {
+            if (loggerConfiguration == null)
+                throw new ArgumentNullException(paramName: nameof(loggerConfiguration));
+
+            return loggerConfiguration.Sink(
+                logEventSink: (ILogEventSink)new TelegramSink(
+                    chatId: chatId,
+                    token: token,
+                    renderMessageImplementation: renderMessageImplementation,
+                    formatProvider: formatProvider
+                ),
+                restrictedToMinimumLevel: restrictedToMinimumLevel);
+        }
+    }
+}
